50215e6ee96bfa83499efff2d09deff2fb26d86c
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-05-22 16:37:34 +0700

    Added: loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,13 @@
+import styles from '@/app/page.module.css'
+
+const Loading = () => {
+  return (
+    <main className={styles.loading}>
+      <span>
+        💀
+      </span>
+    </main>
+  )
+}
+
+export default Loading
