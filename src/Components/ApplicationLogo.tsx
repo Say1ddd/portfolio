c3db8bf7f832c1325977f14c7fd67590f76284a2
Say1ddd <sajidmuhammadikhlas@gmail.com>
2023-05-23 16:38:35 +0700

    Added: ApplicationLogo component

---
+++ src/Components/ApplicationLogo.tsx
@@ -0,0 +1,13 @@
+type ApplicationLogoProps = {
+  className?: string
+}
+
+const ApplicationLogo = ({className}: ApplicationLogoProps) => {
+  return (
+    <div className={className}>
+        <span></span>
+    </div>
+  )
+}
+
+export default ApplicationLogo
